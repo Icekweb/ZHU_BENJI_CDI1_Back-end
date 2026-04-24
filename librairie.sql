d75b236f47b64dacc24b2d209c67e65cb2e40a4c
Icekweb <icekweb@gmail.com>
2026-04-24 23:32:46 +0200

    Add files via upload

---
+++ librairie.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 6.0.0-dev+20260412.9edf12e957
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 24, 2026 at 09:31 PM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `librairie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `livres`
+--
+
+CREATE TABLE `livres` (
+  `id` int NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `auteur` varchar(255) NOT NULL,
+  `annee_publication` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `livres`
+--
+
+INSERT INTO `livres` (`id`, `titre`, `auteur`, `annee_publication`) VALUES
+(1, 'Harry Potter à l\'école des sorciers', 'J.K. Rowling', 1997),
+(2, 'Twilight', 'Stephenie Meyer', 2005),
+(3, 'Hunger Games', 'Suzanne Collins', 2008),
+(4, 'Le Seigneur des Anneaux', 'J.R.R. Tolkien', 1954);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `livres`
+--
+ALTER TABLE `livres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `livres`
+--
+ALTER TABLE `livres`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
